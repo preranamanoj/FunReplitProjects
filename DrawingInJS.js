711876d62c9ea1a2186e7466eb9e5d8979464c0f
Prerana Manoj <97762311+preranamanoj@users.noreply.github.com>
2023-01-20 21:26:40 -0500

    Creating a Drawing Game (Javascript)
    An update to starting a journey in learning Javascript for web and graphical design on Replit with tutorials!

---
+++ DrawingInJS.js
@@ -0,0 +1,12 @@
+let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  noStroke()
+  fill(random(colorlist));//pulling random colours from the colour list that we defined above 
+  ellipse(mouseX, mouseY, 25, 25); //width and height of the ellipse (the circle)- Mouse x and y represent those two factors 
+}
